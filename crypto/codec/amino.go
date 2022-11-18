a3d4063391278c1da6e001ca2435f781bd91dc9e
Joe Abbey <joe.abbey@gmail.com>
2022-11-18 04:21:22 -1000

    Let's try Priority to disambiguate amino Interfaces

--- crypto/codec/amino.go
+++ crypto/codec/amino.go
@@ -3,6 +3,8 @@ package codec
 import (
 	"github.com/tendermint/tendermint/crypto/sr25519"
 
+	"github.com/tendermint/go-amino"
+
 	"github.com/cosmos/cosmos-sdk/codec"
 	"github.com/cosmos/cosmos-sdk/crypto/keys/ed25519"
 	kmultisig "github.com/cosmos/cosmos-sdk/crypto/keys/multisig"
@@ -13,7 +15,19 @@ import (
 // RegisterCrypto registers all crypto dependency types with the provided Amino
 // codec.
 func RegisterCrypto(cdc *codec.LegacyAmino) {
-	cdc.RegisterInterface((*cryptotypes.PubKey)(nil), nil)
+	pubOptions := amino.InterfaceOptions{
+		Priority: []string{
+			"tendermint/PubKeySecp256k1",
+			"injective/PubKeyEthSecp256k1",
+		},
+	}
+	privOptions := amino.InterfaceOptions{
+		Priority: []string{
+			"tendermint/PrivKeySecp256k1",
+			"injective/PrivKeyEthSecp256k1",
+		},
+	}
+	cdc.RegisterInterface((*cryptotypes.PubKey)(nil), &pubOptions)
 	cdc.RegisterConcrete(sr25519.PubKey{},
 		sr25519.PubKeyName, nil)
 	cdc.RegisterConcrete(&ed25519.PubKey{},
@@ -23,7 +37,7 @@ func RegisterCrypto(cdc *codec.LegacyAmino) {
 	cdc.RegisterConcrete(&kmultisig.LegacyAminoPubKey{},
 		kmultisig.PubKeyAminoRoute, nil)
 
-	cdc.RegisterInterface((*cryptotypes.PrivKey)(nil), nil)
+	cdc.RegisterInterface((*cryptotypes.PrivKey)(nil), &privOptions)
 	cdc.RegisterConcrete(sr25519.PrivKey{},
 		sr25519.PrivKeyName, nil)
 	cdc.RegisterConcrete(&ed25519.PrivKey{}, //nolint:staticcheck
